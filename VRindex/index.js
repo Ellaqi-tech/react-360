a43eec341bac66bace79182a20d2f51fa006665e
Ellaqi <ellaqi77777@gmail.com>
2020-05-28 21:53:08 -0400

    remove unnecessary files

---
+++ VRindex/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-360';
+
+export default class VRindex extends React.Component {
+  render() {
+    return (
+      <View style={styles.panel}>
+        <View style={styles.greetingBox}>
+          <Text style={styles.greeting}>
+            Good designs, not only beautiful.
+          </Text>
+        </View>
+      </View>
+    );
+  }
+};
+
+const styles = StyleSheet.create({
+  panel: {
+    // Fill the entire surface
+    width: 1000,
+    height: 600,
+    backgroundColor: 'rgba(255, 255, 255, 0.4)',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  greetingBox: {
+    padding: 20,
+    backgroundColor: '#000000',
+    //borderColor: '#639dda',
+    //borderWidth: 2,
+    //border: none
+  },
+  greeting: {
+    fontSize: 30,
+  },
+});
+
+AppRegistry.registerComponent('VRindex', () => VRindex);
